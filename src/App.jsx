9f7f3e7e9ae6e27ef7eecf4f75212d939afc9bba
Shafi Ahmad <zahain@yahoo.com>
2026-02-15 02:23:04 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HelloWorld from './HelloWorld'
+
+function App() {
+
+  return (
+    <>
+      <HelloWorld />
+    </>
+  )
+}
+
+export default App
---
+++ src/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Welcome to react-js hooks</h1>
+    </div>
+  )
+}
+
+export default HelloWorld
