16a1577435a47a67878fc66c71e79ad6dc23d1da
Shafi Ahmad <zahain@yahoo.com>
2026-02-16 15:21:42 +0530

    add manual deployment button on github repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import HelloWorld from './HelloWorld'
 import Home from './Home'
+import ManualDeploye from './ManualDeploye'
 
 function App() {
 
   return (
     <>
+      <ManualDeploye />
       <HelloWorld />
       <Home />
+
     </>
   )
 }
---
+++ src/ManualDeploye.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ManualDeploye = () => {
+  return (
+    <div>
+      <h1>Welcome to Manual deployement on gitub for production level</h1>
+    </div>
+  )
+}
+
+export default ManualDeploye
